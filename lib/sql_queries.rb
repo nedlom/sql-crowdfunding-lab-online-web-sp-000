f7dc2580409818630e4a213455dbcb9db4316c35
imperative-scientist-4600 <nelom3@gmail.com>
2021-05-17 13:30:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -53,5 +53,6 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
   FROM projects
   INNER JOIN pledges
   ON projects.id = pledges.project_id
+  GROUP BY projects.category
   HAVING projects.category = 'books'"
 end
